d09dc015a55eb0b7e7307034b380358630ba0c29
William Joyce <william.entschluss@gmail.com>
2025-11-17 16:34:47 +0900

    Add user prompting and exit error

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,11 +10,16 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
+    prompt = sys.argv
+    if len(prompt) <= 1:
+        print("no prompt provided")
+        sys.exit(1)
+    user_prompt = " ".join(prompt[1:])
+
     print("Hello from build-ai-agent!")
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=["Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."]
+        model="gemini-2.0-flash-001", contents=user_prompt
     )
-
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
