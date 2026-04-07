3cd5292b63b420550c25c018161420112f927bb4
William Joyce <william.entschluss@gmail.com>
2026-04-07 16:55:29 +0900

    Add file reading

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+
+        valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        target_dir_files = []
+        for file in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, file)
+            file_size = os.path.getsize(file_path)
+            dir_flag = os.path.isdir(file_path)
+            target_dir_files.append(f'- {file}: file_size={file_size} bytes, is_dir={dir_flag}')
+        files_str = '\n'.join(target_dir_files)
+        return files_str
+    except Exception as e:
+        return f'Error: {e}'
+
