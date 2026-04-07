1575a2b00a7958b8232cf8180e83032b8cca2322
William Joyce <william.entschluss@gmail.com>
2026-04-07 19:22:05 +0900

    Add write access

---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        
+        valid_target_path = os.path.commonpath([abs_working_dir, target_file_path]) == abs_working_dir
+        if not valid_target_path:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(target_file_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        
+        os.makedirs(os.path.dirname(target_file_path), mode=0o777, exist_ok=True)
+
+        with open(target_file_path, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error: {e}'
+
