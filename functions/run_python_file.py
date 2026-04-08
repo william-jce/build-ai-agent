f6add14b008841bb93d5f8ea6e9130a46f31891b
William Joyce <william.entschluss@gmail.com>
2026-04-08 16:24:20 +0900

    Add ability to run Python code

---
+++ functions/run_python_file.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
+        valid_target_path = os.path.commonpath([abs_working_dir, target_file_path]) == abs_working_dir
+        if not valid_target_path:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_file_path):
+            return f'Error: "{file_path}" does not exist or it is not a regular file'
+        if os.path.splitext(target_file_path)[1] != ".py":
+            return f'Error: "{file_path}" is not a Python file'
+
+        command = ["python", target_file_path]
+        if args is not None:
+            command.extend(args)
+        complete_comm = subprocess.run(command, cwd=abs_working_dir, capture_output=True,  timeout=30, text=True)
+
+        output_str = []
+        if complete_comm.returncode != 0:
+            output_str.append(f'Process exited with code {complete_comm.returncode}')
+        if not complete_comm.stdout and not complete_comm.stderr:
+            output_str.append('No output produced')
+        else:
+            output_str.append(f'STDOUT: {complete_comm.stdout}')
+            output_str.append(f'STDERR: {complete_comm.stderr}')
+        return "\n".join(output_str)
+    except Exception as e:
+        return f'Error: {e}'
